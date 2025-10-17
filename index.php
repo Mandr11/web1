b5e7c5b1db4db1a93bcbc93cec5d9f6501b0600d
Mandri <ado79144@gmail.com>
2025-10-17 10:11:26 +0700

    Update header text in index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "<h1>Web1 - Aplikasi 1 (Test)</h1>";
+echo "<h1>Web1 - Aplikasi 1 git (Test)</h1>";
 echo "<p>Server IP: " . ($_SERVER['SERVER_ADDR'] ?? 'unknown') . " | Port: 2000</p>";
 echo "<p>Waktu server: " . date('Y-m-d H:i:s') . "</p>";
 ?>
