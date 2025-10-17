e1e04f04066079b49142a42a938cb33b12b60be2
Mandri <ado79144@gmail.com>
2025-10-17 10:09:09 +0700

    Add basic PHP server information display

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1>Web1 - Aplikasi 1 (Test)</h1>";
+echo "<p>Server IP: " . ($_SERVER['SERVER_ADDR'] ?? 'unknown') . " | Port: 2000</p>";
+echo "<p>Waktu server: " . date('Y-m-d H:i:s') . "</p>";
+?>
