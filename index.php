be8a9b81b19eabeeeaa2607f94dac486a6f8b3b4
Mandri <ado79144@gmail.com>
2025-10-17 14:34:40 +0700

    Update paragraph text in index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 echo "<h1>Web1 - Aplikasi 1 </h1>";
-echo "<p>test ubah dari github</p>";
+echo "<p>test ubah dari github  web 1</p>";
 echo "<p>Server IP: " . ($_SERVER['SERVER_ADDR'] ?? 'unknown') . " | Port: 2000</p>";
 echo "<p>Waktu server: " . date('Y-m-d H:i:s') . "</p>";
 ?>
